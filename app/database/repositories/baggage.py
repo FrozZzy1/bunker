ac27be76f19eba2b5c19fbd1c7042953bd78658e
frozzzy1 <maleev.av@dclouds.ru>
2024-11-13 23:50:14 +0700

    feat: add baggage

---
+++ app/database/repositories/baggage.py
@@ -0,0 +1,17 @@
+from sqlalchemy import select
+
+from app.api.schemas.baggage import AddBaggageSchema
+from app.database.models.baggage import BaggageOrm
+from app.utils.repository import AbsRepo
+
+
+class BaggageRepository(AbsRepo):
+    async def add_one(self, data: AddBaggageSchema) -> None:
+        baggage = BaggageOrm(**data.model_dump())
+        self.session.add(baggage)
+        await self.session.commit()
+
+    async def get_all(self) -> list[BaggageOrm]:
+        query = select(BaggageOrm)
+        result = self.session.scalars(query)
+        return result
